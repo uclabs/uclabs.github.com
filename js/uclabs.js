05fc7ad42f7f4431c479e5ce8a4c2a116272a842
nino zhang <ninozhang@foxmail.com>
2012-10-30 23:52:53 +0800

    各种更新

---
+++ js/uclabs.js
@@ -0,0 +1,59 @@
+var data = [
+    {
+        thumb: 'screenshot/battery/thumbnail.jpg',
+        image: 'screenshot/battery/thumbnail.jpg',
+        big: 'big.jpg',
+        title: 'My title',
+        description: 'My description',
+        link: 'http://my.destination.com',
+        layer: '<div><h2>This image is gr8</h2><p>And this text will be on top of the image</p>'
+    },
+    {
+        thumb: 'screenshot/day-night/thumbnail.jpg',
+        image: 'screenshot/day-night/thumbnail.jpg',
+        big: 'big.jpg',
+        title: 'My title',
+        description: 'My description',
+        link: 'http://my.destination.com',
+        layer: '<div><h2>This image is gr8</h2><p>And this text will be on top of the image</p>'
+    },
+    {
+        thumb: 'screenshot/gravity-ball/thumbnail.jpg',
+        image: 'screenshot/gravity-ball/thumbnail.jpg',
+        big: 'big.jpg',
+        title: 'My title',
+        description: 'My description',
+        link: 'http://my.destination.com',
+        layer: '<div><h2>This image is gr8</h2><p>And this text will be on top of the image</p>'
+    },
+    {
+        thumb: 'screenshot/image-wall/thumbnail.jpg',
+        image: 'screenshot/image-wall/thumbnail.jpg',
+        big: 'big.jpg',
+        title: 'My title',
+        description: 'My description',
+        link: 'http://my.destination.com',
+        layer: '<div><h2>This image is gr8</h2><p>And this text will be on top of the image</p>'
+    },
+    {
+        thumb: 'screenshot/marquee/thumbnail.jpg',
+        image: 'screenshot/marquee/thumbnail.jpg',
+        big: 'big.jpg',
+        title: 'My title',
+        description: 'My description',
+        link: 'http://my.destination.com',
+        layer: '<div><h2>This image is gr8</h2><p>And this text will be on top of the image</p>'
+    },
+    {
+        thumb: 'screenshot/vibrator/thumbnail.jpg',
+        image: 'screenshot/vibrator/thumbnail.jpg',
+        big: 'big.jpg',
+        title: 'My title',
+        description: 'My description',
+        link: 'http://my.destination.com',
+        layer: '<div><h2>This image is gr8</h2><p>And this text will be on top of the image</p>'
+    }
+];
+Galleria.run('#demo-screenshot', {
+    dataSource: data
+});
